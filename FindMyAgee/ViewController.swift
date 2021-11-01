84e0c1c789adc50ef202b08c73dd9d5b1501eb11
Rizky28eka <eka28rizky03haryadi04@gmail.cim>
2021-11-01 22:07:49 +0700

    Initial Commit

---
+++ FindMyAgee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindMyAgee
+//
+//  Created by Rizky Eka Haryadi on 01/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
